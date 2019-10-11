2ee2aca346ab7bdb3499f1c2f60b1f50a4b611a3
Sergio Ramos Santonja <sergioramos55@outlook.es>
2019-10-11 11:44:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/radiobutton/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.radiobutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup rg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rg = (RadioGroup)findViewById(R.id.radioGroup);
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                Toast.makeText(getApplicationContext(), "Opcion chequedada" + i , Toast.LENGTH_LONG ).show();
+            }
+        });
+    }
+}
